1f810af69971817f593f4aee593686f1cb77ccea
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-12-20 17:35:25 +0530

    add shop category

--- e-commerce/src/Pages/ShopCategory.jsx
+++ e-commerce/src/Pages/ShopCategory.jsx
@@ -1,12 +1,30 @@
 import React, { useContext } from 'react'
 import './CSS/ShopCategory.css'
 import { ShopContext } from '../Context/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon'
+import Item from '../Components/Assets/Item'
 
-function ShopCategory() {
+function ShopCategory(props) {
   const {all_product} = useContext(ShopContext)
   return (
     <div className='shop-category'>
-      
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span>out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map((item, i)=>{
+          if(props.category===item.category)
+          {
+            return<Item></Item>
+          }
+        })}
+      </div>
     </div>
   )
 }
