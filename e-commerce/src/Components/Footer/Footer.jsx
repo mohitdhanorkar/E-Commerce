53a84dcc91dc4260c70e95fc00e167e4d9e6aef2
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2023-12-20 16:53:39 +0530

    styling the footer

--- e-commerce/src/Components/Footer/Footer.jsx
+++ e-commerce/src/Components/Footer/Footer.jsx
@@ -4,10 +4,10 @@ import footer_logo from "../Assets/logo.png";
 import instagram_icon from '../Assets/instagram_icon.png'
 import pintester_icon from '../Assets/pintester_icon.png'
 import whatsapp_icon from '../Assets/whatsapp_icon.png'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  const [link, setLink] = useState()
+  // const [link, setLink] = useState("")
 
   return (
     <div className="footer">
@@ -15,12 +15,20 @@ const Footer = () => {
         <img src={footer_logo} alt="" />
         <p>SHOPPER</p>
       </div>
+      {/* <ul className="footer-links">
+        <li onClick={()=> {setLink("company")}}><Link style={{ textDecoration: 'none'}} to='/company'>Company</Link>{link==="company"?<hr/>:<></>}</li>
+        <li onClick={()=> {setLink("product")}}><Link style={{ textDecoration: 'none'}} to='/product'>Product</Link>{link==="product"?<hr/>:<></>}</li>
+        <li onClick={()=> {setLink("offices")}}><Link style={{ textDecoration: 'none'}} to='/offices'>Offices</Link>{link==="offices"?<hr/>:<></>}</li>
+        <li onClick={()=> {setLink("about")}}><Link style={{ textDecoration: 'none'}} to='/about'>About</Link>{link==="about"?<hr/>:<></>}</li>
+        <li onClick={()=> {setLink("contact")}}><Link style={{ textDecoration: 'none'}} to='/contact'>Contact</Link>{link==="contact"?<hr/>:<></>}</li>
+      </ul> */}
+      
       <ul className="footer-links">
-        <li onClick={()=> {setLink()}}><Link style={{ textDecoration: 'none'}} to='/company'>Company</Link>{link==="company"?<hr/>:<></>}</li>
-        <li onClick={()=> {setLink()}}><Link style={{ textDecoration: 'none'}} to='/product'>Product</Link>{link==="product"?<hr/>:<></>}</li>
-        <li onClick={()=> {setLink()}}><Link style={{ textDecoration: 'none'}} to='/offices'>Offices</Link>{link==="offices"?<hr/>:<></>}</li>
-        <li onClick={()=> {setLink()}}><Link style={{ textDecoration: 'none'}} to='/about'>About</Link>{link==="about"?<hr/>:<></>}</li>
-        <li onClick={()=> {setLink()}}><Link style={{ textDecoration: 'none'}} to='/contact'>Contact</Link>{link==="contact"?<hr/>:<></>}</li>
+        <li>Company</li>
+        <li>Product</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
       </ul>
       <div className="footer-social-icon">
         <div className="footer-icon-container">
