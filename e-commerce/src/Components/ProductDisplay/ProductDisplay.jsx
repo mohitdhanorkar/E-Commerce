29a07bfdf943fa039d25d618ef71f973a50466ed
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2024-01-16 01:12:14 +0530

    add

--- e-commerce/src/Components/ProductDisplay/ProductDisplay.jsx
+++ e-commerce/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './ProductDisplay.css'
 
-const ProductDisplay = () => {
+const ProductDisplay = (props) => {
+    const {product} = props;
   return (
     <div className='productdisplay'>
-      
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+            <img src={product.image} alt="" />
+
+        </div>
+      </div>
     </div>
   )
 }
