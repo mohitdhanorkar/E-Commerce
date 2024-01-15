b589103c365d4c3421bd22c61ead9a9dae345cc0
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2024-01-16 01:02:58 +0530

    add breadcrum in product

--- e-commerce/src/Components/Item/Item.jsx
+++ e-commerce/src/Components/Item/Item.jsx
@@ -4,7 +4,7 @@ import './Item.css'
 function Item(props) {
   return (
     <div className='item'>
-      <img src={props.image} alt="" />
+      <Link><img src={props.image} alt="" /></Link>
       <p>{props.name}</p>
       <div className="item-prices">
         <div className="item-price-new">
