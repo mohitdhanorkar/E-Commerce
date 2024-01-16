84b7607ca9a509b02f909e94b5007768f08caa2d
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2024-01-16 12:49:55 +0530

    add category for women

--- e-commerce/src/Components/ProductDisplay/ProductDisplay.jsx
+++ e-commerce/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -47,6 +47,7 @@ const ProductDisplay = (props) => {
             </div>
         </div>
         <button>ADD TO CART</button>
+        <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
       </div>
     </div>
   )
