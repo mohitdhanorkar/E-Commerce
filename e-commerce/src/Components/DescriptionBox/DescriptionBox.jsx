9d770ef604249a31a6bbbe0ad30a296b18f36bf2
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2024-01-17 00:09:40 +0530

    add structure for description

--- e-commerce/src/Components/DescriptionBox/DescriptionBox.jsx
+++ e-commerce/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -4,7 +4,13 @@ import './DescriptionBox.css'
 const DescriptionBox = () => {
   return (
     <div className='descriptionbox'>
-      
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+      <div className="descriptionbox-description">
+        <p></p>
+      </div>
     </div>
   )
 }
