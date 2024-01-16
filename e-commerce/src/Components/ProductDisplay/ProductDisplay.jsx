7e9a42659f61ed9505124538e7737a7c5cfa0422
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2024-01-16 12:51:14 +0530

    add tags

--- e-commerce/src/Components/ProductDisplay/ProductDisplay.jsx
+++ e-commerce/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -48,6 +48,8 @@ const ProductDisplay = (props) => {
         </div>
         <button>ADD TO CART</button>
         <p className='productdisplay-right-category'><span>Category :</span>Women, T-Shirt, Crop Top</p>
+        <p className='productdisplay-right-category'><span>Tags :</span></p>Morden, Latest
+
       </div>
     </div>
   )
