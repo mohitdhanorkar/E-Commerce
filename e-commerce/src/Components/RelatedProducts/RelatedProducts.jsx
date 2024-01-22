64772c9ee342f14d0c62bac2d88593230b58f394
mohitdhanorkar <dhanorkarmohit2@gmail.com>
2024-01-22 13:47:49 +0530

    changes in products

--- e-commerce/src/Components/RelatedProducts/RelatedProducts.jsx
+++ e-commerce/src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './RelatedProducts.css'
 import data_product from '../Assets/data'
+
 const RelatedProducts = () => {
   return (
     <div className='relatedproducts'>
@@ -8,7 +9,7 @@ const RelatedProducts = () => {
       <hr />
       <div className="relatedproducts-item">
         {data_product.map((item, i)=>{
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+            return <item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
             })}
       </div>
     </div>
